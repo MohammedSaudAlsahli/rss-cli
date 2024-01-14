2103caad07b13545b9975d0ae5a117298c75b62a
Mohammed Alsahli <mohammed.saud.alsahli@gmail.com>
2024-01-14 04:22:48 +0300

    i cant do it :(

--- rss_project/data/rss.py
+++ rss_project/data/rss.py
@@ -4,11 +4,6 @@ from rss_project.data.locations import data_dir
 
 
 def rss_file() -> Path:
-    """The path to the file that the local rss are held in.
-
-    Returns:
-        The path to the rss file.
-    """
     return data_dir() / "rss.json"
 
 
